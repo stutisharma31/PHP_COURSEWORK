44a64ca83e52e132dc855f340fd3428ead756f69
Stuti Sharma <stuti.shashu@gmail.com>
2024-10-21 10:51:54 +0530

    add

--- exceptionhandling.php
+++ exceptionhandling.php
@@ -1,14 +1,15 @@
 <?php
 try
 {
-    $firstValue = 10;
-    $secondValue = 0;
-    if ($secondValue == 0) throw new Exception("Divide by Zero exception occurred");
-    $result = $firstValue / $secondValue;
-    printf("Result is : %d", $result);
+    $p = 10;
+    $q = 0;
+    if ($q == 0) 
+    throw new Exception("Division by zero is not allowed");
+    $a = $p / $q;
+    printf("Result is : %d", $a);
 }
 catch(Exception $e)
 {
-    printf("Exception: %s", $e->getMessage());
+    printf("Error: %s", $e->getMessage());
 }
 ?>
