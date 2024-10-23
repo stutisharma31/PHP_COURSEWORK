5ee5518357bb34c2d3d5608d4e645a9ca1ec6a88
Stuti Sharma <stuti.shashu@gmail.com>
2024-10-23 10:50:39 +0530

    add

--- mysql.php
+++ mysql.php
@@ -8,5 +8,19 @@ if($conn->connect_error){
   die("Connection failed".$conn->connect_error);
 }
 echo "Connection successful";
+echo "<br>";
+$sql="CREATE TABLE IF NOT EXISTS customer(
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  password VARCHAR(20),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )";
+if ($conn->query($sql)===TRUE){
+  echo "Table customer created successfully";
+}else{
+  echo"Table not created".$conn->error;
+}
+
 $conn->close();
 ?>
